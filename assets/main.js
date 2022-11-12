4ea63733f8b43b8a99d41a4b260b218673508f2c
EliaMagris <elia.magris@libero.it>
2022-11-12 18:11:16 +0100

    aggiunta bottoncini

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,10 @@
 var app = new Vue({
-  el: "#root",
+  el: '#root',
   data: {
     img: [
-      { position: "assets/img/30212993894_85351f21ba.jpg" },
-      { position: "assets/img/52c897521aa86a677f446cf95e20d57b.jpg" },
-      { position: "assets/img/39709319980_7b2159e8b5.jpg" },
+      { position: 'assets/img/30212993894_85351f21ba.jpg' },
+      { position: 'assets/img/52c897521aa86a677f446cf95e20d57b.jpg' },
+      { position: 'assets/img/39709319980_7b2159e8b5.jpg' },
     ],
     index: 1,
     timer: null,
@@ -27,11 +27,6 @@ var app = new Vue({
         return (this.index = 2);
       }
       return this.index--;
-    },
-
-    stopRotation: function () {
-      clearTimeout(this.timer);
-      this.timer = null;
     },
 
     img0: function () {
